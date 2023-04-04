65f11831eb3abaa7c475ec8eb720977143d6d3e5
Spazmann <dmann@student.neumont.edu>
2023-04-04 14:53:16 -0600

    Done for now

---
+++ 2_1_DataTypes/Employee.h
@@ -0,0 +1,24 @@
+#pragma once
+class Employee
+{
+	// put proerties at the top
+public:
+	char name[33];
+	int age = 0;
+	float hoursPerDay[7];
+
+protected:
+
+private:
+
+	// Put methods in the middle
+public:
+	void Read();
+	void Write();
+
+protected:
+
+private:
+
+};
+
